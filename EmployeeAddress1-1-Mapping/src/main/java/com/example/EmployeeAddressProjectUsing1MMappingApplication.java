c5b0c7957d7661e4ebc669d4e060079e9ef81765
Yashpal Rajput <yashpalgirase692@gmail.com>
2023-07-22 21:55:56 +0530

    This is my first commit

---
+++ EmployeeAddress1-1-Mapping/src/main/java/com/example/EmployeeAddressProjectUsing1MMappingApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAddressProjectUsing1MMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAddressProjectUsing1MMappingApplication.class, args);
+	}
+
+}
